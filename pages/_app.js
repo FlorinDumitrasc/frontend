b3ae59d9524a3a8ac5abbaf380dc3dbb362f0efb
FlorinDumitrasc <florind.dev@gmail.com>
2022-08-06 17:48:24 +0300

    fullstack ecommerce

--- pages/_app.js
+++ pages/_app.js
@@ -1,7 +1,15 @@
-import '../styles/globals.css'
+import "../styles/globals.css";
+import { Provider, createClient } from "urql";
+
+const backendApi = process.env.NEXT_PUBLIC_BACKEND_API;
+const client = createClient({ url: backendApi });
 
 function MyApp({ Component, pageProps }) {
-  return <Component {...pageProps} />
+  return (
+    <Provider value={client}>
+      <Component {...pageProps} />
+    </Provider>
+  );
 }
 
-export default MyApp
+export default MyApp;
